5b35993e40b2e9fdb22d031f8d2a29fe273bb00d
Gabor Javorszky <gabor@javorszky.co.uk>
2018-03-24 19:04:56 +0000

    Remove unused code and tests

--- salt.go
+++ salt.go
@@ -50,29 +50,17 @@ func init() {
 
 // GiveSalts responds to the GET / request
 func GiveSalts(c echo.Context) error {
-	// return c.String(http.StatusOK, GenerateSaltsWP8x64())
 	return c.String(http.StatusOK, GenerateSaltsWP512())
 }
 
 // GiveSaltsEnv responds to the GET /env request
 func GiveSaltsEnv(c echo.Context) error {
-	// return c.String(http.StatusOK, GenerateSaltsEnv8x64())
 	return c.String(http.StatusOK, GenerateSaltsEnv512())
 }
 
 // GiveSaltsJSON responds to the GET /json request
 func GiveSaltsJSON(c echo.Context) error {
 	return c.JSON(http.StatusOK, GenerateSaltsJSON512())
-}
-
-// GenerateSaltsWP8x64 generates the content for GiveSalts by calling the method 8x
-func GenerateSaltsWP8x64() string {
-	formattedStrings := make([]string, 8)
-
-	for i, arg := range saltTypes {
-		formattedStrings[i] = fmt.Sprintf("define( '%s',%s'%s' );", arg, strings.Repeat(" ", 17-len(arg)), RandStringBytesMaskImprSrc(64))
-	}
-	return strings.Join(formattedStrings, "\n")
 }
 
 // GenerateSaltsWP512 generates the content for GiveSalts by calling the method once and slicing
@@ -82,16 +70,6 @@ func GenerateSaltsWP512() string {
 
 	for i, arg := range saltTypes {
 		formattedStrings[i] = fmt.Sprintf("define( '%s',%s'%s' );", arg, strings.Repeat(" ", 17-len(arg)), longstring[i*64:(i+1)*64])
-	}
-	return strings.Join(formattedStrings, "\n")
-}
-
-// GenerateSaltsEnv8x64 generates the content for GiveSaltsEnv by calling the method 8x
-func GenerateSaltsEnv8x64() string {
-	formattedStrings := make([]string, 8)
-
-	for i, arg := range saltTypes {
-		formattedStrings[i] = fmt.Sprintf("%s=\"%s\"", arg, RandStringBytesMaskImprSrc(64))
 	}
 	return strings.Join(formattedStrings, "\n")
 }
@@ -118,63 +96,6 @@ func GenerateSaltsJSON512() map[string]string {
 	return formattedStrings
 }
 
-// AUTH_KEY="put secure salts here"
-// SECURE_AUTH_KEY="put secure salts here"
-// LOGGED_IN_KEY="put secure salts here"
-// NONCE_KEY="put secure salts here"
-// AUTH_SALT="put secure salts here"
-// SECURE_AUTH_SALT="put secure salts here"
-// LOGGED_IN_SALT="put secure salts here"
-// NONCE_SALT="put secure salts here"
-
-// define('AUTH_KEY',         'H!R+>kY|$Nf|Nx%ElcRs; 1?;7[JH63-`F!)jVJ<,&jLY,{f+spcv+r+hRR6tVrA');
-// define('SECURE_AUTH_KEY',  'qWtn8!1pN.20WMoh_U-yCPu>.fi@PfIMSq2`mR!#H[32m8QT-q|R<O%,i+e~T+yr');
-// define('LOGGED_IN_KEY',    'M6~Cg)qsd}ke(nlb= ;0Qu:+b:+C6n&30ngvGq-k!yj^Xs>{XBwpj-x!brRmR??F');
-// define('NONCE_KEY',        'Jx]|.<)WzY4grFWnlmGdI,`(v#cxTvoOtbrlV-$5]r:z|J)9Ouc,Go .YBW{v^nk');
-// define('AUTH_SALT',        'L^D-sxa}@iu{*r3i+|qm;,-l/7Po+S?P<=iCMqh;,EJ/7D8`[1yj]: vkLF-^:f1');
-// define('SECURE_AUTH_SALT', '`l_vCu1htqxQ+i8+15$XfFuBzj65l!pAT=x^|PL8,Le%Kq`[wY)km e-p|4r~rws');
-// define('LOGGED_IN_SALT',   '5&nNK-aXkIhFD01b&EJ#L>JeW3]%{((vBtnst.`X_~<w&lO+bpQtvW@A7GSu(dA,');
-// define('NONCE_SALT',       'wwsQ#<zh3+y> 1o`VH,db>%MD7|qG-:c_f!#.OuBp|)<%rRz.jscc[}=q>T5-TpZ');
-
-// RandStringRunes generates random string runes
-func RandStringRunes(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
-	}
-	return string(b)
-}
-
-// RandStringBytes generates random string from bytes
-func RandStringBytes(n int) string {
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
-	}
-	return string(b)
-}
-
-// RandStringBytesRmndr generates random string from the remainder from dividing a 64 bit number with the 64 bit representation of the length of characters
-func RandStringBytesRmndr(n int) string {
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
-	}
-	return string(b)
-}
-
-// RandStringBytesMask only uses the last 7 bits of the generated 63 byte int
-func RandStringBytesMask(n int) string {
-	b := make([]byte, n)
-	for i := 0; i < n; {
-		if idx := int(rand.Int63() & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i++
-		}
-	}
-	return string(b)
-}
-
 // RandStringBytesMaskImpr slices up the bits of the random number and uses all slices
 func RandStringBytesMaskImpr(n int) string {
 	b := make([]byte, n)
@@ -182,25 +103,6 @@ func RandStringBytesMaskImpr(n int) string {
 	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
 			cache, remain = rand.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
-	}
-
-	return string(b)
-}
-
-// RandStringBytesMaskImprSrc slices the rand number and uses a rand.Source instead of a rand.Rand
-func RandStringBytesMaskImprSrc(n int) string {
-	b := make([]byte, n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
 		}
 		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
 			b[i] = letterBytes[idx]
--- salt_test.go
+++ salt_test.go
@@ -2,91 +2,6 @@ package main
 
 import "testing"
 
-// from salt.go
-func BenchmarkRandStringRunes8x64(b *testing.B) {
-	// run the RandStringRunes function b.N times
-	for n := 0; n < b.N; n++ {
-		RandStringRunes(64)
-		RandStringRunes(64)
-		RandStringRunes(64)
-		RandStringRunes(64)
-		RandStringRunes(64)
-		RandStringRunes(64)
-		RandStringRunes(64)
-		RandStringRunes(64)
-	}
-}
-
-func BenchmarkRandStringRunes512(b *testing.B) {
-	// run the RandStringRunes function b.N times
-	for n := 0; n < b.N; n++ {
-		RandStringRunes(512)
-	}
-}
-
-func BenchmarkRandStringBytes8x64(b *testing.B) {
-	// run the RandStringBytes function b.N times
-	for n := 0; n < b.N; n++ {
-		RandStringBytes(64)
-		RandStringBytes(64)
-		RandStringBytes(64)
-		RandStringBytes(64)
-		RandStringBytes(64)
-		RandStringBytes(64)
-		RandStringBytes(64)
-		RandStringBytes(64)
-	}
-}
-
-func BenchmarkRandStringBytes512(b *testing.B) {
-	// run the RandStringBytes function b.N times
-	for n := 0; n < b.N; n++ {
-		RandStringBytes(512)
-	}
-}
-
-func BenchmarkRandStringBytesRmndr8x64(b *testing.B) {
-	// run the RandStringBytesRmndr function b.N times
-	for n := 0; n < b.N; n++ {
-		RandStringBytesRmndr(64)
-		RandStringBytesRmndr(64)
-		RandStringBytesRmndr(64)
-		RandStringBytesRmndr(64)
-		RandStringBytesRmndr(64)
-		RandStringBytesRmndr(64)
-		RandStringBytesRmndr(64)
-		RandStringBytesRmndr(64)
-	}
-}
-
-func BenchmarkRandStringBytesRmndr512(b *testing.B) {
-	// run the RandStringBytesRmndr function b.N times
-	for n := 0; n < b.N; n++ {
-		RandStringBytesRmndr(512)
-	}
-}
-
-func BenchmarkRandStringBytesMask8x64(b *testing.B) {
-	// run the RandStringBytesMask function b.N times
-	for n := 0; n < b.N; n++ {
-		RandStringBytesMask(64)
-		RandStringBytesMask(64)
-		RandStringBytesMask(64)
-		RandStringBytesMask(64)
-		RandStringBytesMask(64)
-		RandStringBytesMask(64)
-		RandStringBytesMask(64)
-		RandStringBytesMask(64)
-	}
-}
-
-func BenchmarkRandStringBytesMask512(b *testing.B) {
-	// run the RandStringBytesMask function b.N times
-	for n := 0; n < b.N; n++ {
-		RandStringBytesMask(512)
-	}
-}
-
 func BenchmarkRandStringBytesMaskImpr8x64(b *testing.B) {
 	// run the RandStringBytesMaskImpr function b.N times
 	for n := 0; n < b.N; n++ {
@@ -108,48 +23,21 @@ func BenchmarkRandStringBytesMaskImpr512(b *testing.B) {
 	}
 }
 
-func BenchmarkRandStringBytesMaskImprSrc8x64(b *testing.B) {
-	// run the RandStringBytesMaskImprSrc function b.N times
-	for n := 0; n < b.N; n++ {
-		RandStringBytesMaskImprSrc(64)
-		RandStringBytesMaskImprSrc(64)
-		RandStringBytesMaskImprSrc(64)
-		RandStringBytesMaskImprSrc(64)
-		RandStringBytesMaskImprSrc(64)
-		RandStringBytesMaskImprSrc(64)
-		RandStringBytesMaskImprSrc(64)
-		RandStringBytesMaskImprSrc(64)
-	}
-}
-
-func BenchmarkRandStringBytesMaskImprSrc512(b *testing.B) {
-	// run the RandStringBytesMaskImprSrc function b.N times
-	for n := 0; n < b.N; n++ {
-		RandStringBytesMaskImprSrc(512)
-	}
-}
-
-func BenchmarkGenerateSaltsWP8x64(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		GenerateSaltsWP8x64()
-	}
-}
-
 func BenchmarkGenerateSaltsWP512(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		GenerateSaltsWP512()
 	}
 }
 
-func BenchmarkGenerateSaltsEnv8x64(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		GenerateSaltsEnv8x64()
-	}
-}
-
 func BenchmarkGenerateSaltsEnv512(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		GenerateSaltsEnv512()
+	}
+}
+
+func BenchmarkGenerateSaltsJSON512(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		GenerateSaltsJSON512()
 	}
 }
 
